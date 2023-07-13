d964076a5eead38100ec5013cc9b9e06741e8d91
rashul chutani <rashul.chutani@gmail.com>
2023-07-13 11:14:06 -0400

    Add Caching for Initial Module Data Loading

---
+++ server/cache.js
@@ -0,0 +1,19 @@
+// The cache object should store both the data and the timestamp of when it was fetched.
+let cache = {
+  data: [],
+  fetchTimestamp: null,
+};
+
+function setData(newData) {
+  cache = {
+    data: newData,
+    fetchTimestamp: Date.now(),
+  };
+}
+
+module.exports = {
+  getCache: function () {
+    return cache.data;
+  },
+  setCache: setData,
+};
